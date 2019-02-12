d5514a8644da6ac3d5cab04f63861ae3887e7b8d
Maiker <m2ikr23@gmail.com>
2019-02-12 03:57:02 -0800

    modificaciones en el mapa

--- resources/views/vendor/spark/dashboard/maps.blade.php
+++ resources/views/vendor/spark/dashboard/maps.blade.php
@@ -1,4 +1,4 @@
 
-<div class="bl-primary">
-   <div id="map"></div>
+<div class="bl-primary" style="width:165%; height: 100%;margin-left: -5% ">
+   <div id="map" style="width:165%; margin-left: 0 "></div>
 </div>
