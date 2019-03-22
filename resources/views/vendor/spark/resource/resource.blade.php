43a54bd5419c0e592aad674020b565f518e81200
Maiker <m2ikr23@gmail.com>
2019-03-21 22:42:12 -0400

    configuracion de recursos completa backend  y frontend

--- resources/views/vendor/spark/resource/resource.blade.php
+++ resources/views/vendor/spark/resource/resource.blade.php
@@ -4,11 +4,14 @@
             <div role="tabpanel" id="pond-detail-pills">
                 <!-- Nav tabs -->
                 <ul class="nav nav-pills nav-tabs" role="tablist">
-                    <li role="presentation" class=" nav-item"><a class="btn btn-light nav-link active" href="#resource"
+                    <li role="resource" class=" nav-item"><a class="btn btn-light nav-link active" href="#resource"
                             aria-controls="resource_section" role="tab" data-toggle="tab">Recursos</a></li>
-                    <li role="presentation"><a class="btn btn-light" href="#providers" aria-controls="providers_section"
+                    <li role="presentation" class=" nav-item"><a class="btn btn-light nav-link" href="#presentation"
+                            aria-controls="resource_section" role="tab" data-toggle="tab">Presentaciones</a></li>
+                   
+                    <li role="providers"><a class="btn btn-light" href="#providers" aria-controls="providers_section"
                             role="tab" data-toggle="tab">Proveedores</a></li>
-                    <li role="presentation"><a class="btn btn-light" href="#labs" aria-controls="labs_section"
+                    <li role="labs"><a class="btn btn-light" href="#labs" aria-controls="labs_section"
                             role="tab" data-toggle="tab">Laboratorios</a></li>
                 </ul>
             </div>
@@ -17,26 +20,24 @@
                 <div role="tabpanel" class="tab-pane active" id="resource">
                     <div class="mid_container">
                         <section class="section">
-                            <div class=" " id="feed_table" style="overflow: hidden;">
-                                <div class="pull-left search mb-2">
-                                    <input class="form-control" type="text" placeholder="Buscar">
-                                </div>
-                                <div class="pull-right search mb-2">
+                                <div class="btn-tools-bar">
                                     <button type="button" id="addfood" class="btn btn-info ml-5" data-toggle="modal"
                                         data-target="#addResourceModal"><i class="fa fa-plus" aria-hidden="true"></i>
                                         Añadir Recurso</button>
                                 </div>
-                                <table class="table table-striped table-hover bg-white text-center" id="feedSch_tbl"
+                                <table class="bg-white"
+                                    data-toggle="table"
+                                    data-classes="table table-striped table-hover table-borderless"
+                                    data-pagination="true"
+                                    data-search="true"
                                     style="width:99.9%;">
                                     <thead class="thead-primary">
 
                                         <tr>
-                                            <th>Nombre del Recurso</th>
+                                            <th data-sortable="true"
+                                                data-field="name">Nombre del Recurso</th>
                                             <th class="">Proveedor</th>
                                             <th class="">Tipo</th>
-                                            <th class="">Presentación</th>
-                                            <th class="">Cantidad</th>
-                                            <th class="">Precio</th>
                                             <th class="">Acciones</th>
                                         </tr>
                                     </thead>
@@ -46,17 +47,82 @@
                                             <td>{{$resource->name}}</td>
                                             <td>{{$resource->provider_name}}</td>
                                             <td>{{$resource->category_name}}</td>
-                                            <td >{{$resource->presentation_name}}</td>
-                                            <td>{{$resource->quantity}}-{{$resource->unity}}</td>
-                                            <td>{{$resource->price}}</td>
                                             <td class="text-center">
                                                 <div class="actions btn btn-group-sm">
                                                     <button id="#edit" data-toggle="modal"
                                                         data-target="#editResourceModal"
+                                                        data-id="{{$resource->id}}"
+                                                        data-name="{{$resource->name}}"
+                                                        data-provider_id="{{$resource->provider_id}}"
+                                                        data-category_id="{{$resource->category_id}}"
                                                         class="btn btn-success btn-xs mr-4">
                                                         <i class="fa fa-edit"></i></button>
-                                                    <button data-toggle="modal" data-target="#deleteResourceModal"
+                                                    <button data-toggle="modal"
+                                                        data-target="#deleteResourceModal"
+                                                        data-id="{{$resource->id}}"
                                                         class="btn btn-xs btn-danger">
+                                                        <i class="fa fa-trash-o"></i></button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                        </section>
+                    </div>
+                    <!-- Modals resources -->
+                    @include('spark::modals.resource.ResourceModals')
+                </div>
+                 <!-- tab presentation-->
+                <div role="tabpanel" class="tab-pane" id="presentation">
+                    <div class="mid_container">
+                        <section class="section">
+                            <div class=" " id="feed_table" style="overflow: hidden;">
+                                <div class="btn-tools-bar">
+                                    <button type="button" id="addfood" class="btn btn-info ml-5" data-toggle="modal"
+                                        data-target="#addPresentationModal"><i class="fa fa-plus" aria-hidden="true"></i>
+                                        Añadir presentación</button>
+                                </div>
+                                <table class="bg-white"
+                                    data-toggle="table"
+                                    data-classes="table table-striped table-hover table-borderless"
+                                    data-pagination="true"
+                                    data-search="true">
+                                    <thead class="thead-primary">
+
+                                        <tr>
+                                            <th data-sortable="true"
+                                                data-field="name" >Nombre de recurso</th>
+                                            <th class="">Presentación</th>
+                                            <th class="">Cantidad</th>
+                                            <th class="">Precio</th>
+                                            <th class="">Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="results" aria-live="polite" aria-relevant="all">
+                                    @foreach ($presentations as $presentation)
+                                        <tr>
+                                            <td>{{$presentation->resource_name}}</td>
+                                            <td >{{$presentation->name}}</td>
+                                            <td>{{$presentation->quantity}} {{$presentation->unity}}</td>
+                                            <td>{{$presentation->price}}</td>
+                                            <td class="text-center">
+                                                <div class="actions btn btn-group-sm">
+                                                    <button id="#edit" data-toggle="modal"
+                                                        data-target="#editPresentationModal"
+                                                        data-id="{{$presentation->id}}"
+                                                        data-resource_id="{{$presentation->resource_id}}"
+                                                        data-name ="{{$presentation->name}}"
+                                                        data-quantity="{{$presentation->quantity}}"
+                                                        data-price="{{$presentation->price}}"
+                                                        data-unity="{{$presentation->unity}}"
+                                                        class="btn btn-success btn-xs mr-4">
+                                                        <i class="fa fa-edit"></i></button>
+                                                    <button 
+                                                    data-toggle="modal"
+                                                    data-target="#deletePresentationModal"
+                                                    data-id="{{$presentation->id}}"
+                                                    class="btn btn-xs btn-danger">
                                                         <i class="fa fa-trash-o"></i></button>
                                                 </div>
                                             </td>
@@ -68,25 +134,23 @@
                             </div>
                         </section>
                     </div>
-                    <!-- Modals resources -->
-                    @include('spark::modals.resource.ResourceModals')
+                    <!-- Modals presentation -->
+                    @include('spark::modals.resource.presentationModals')
                 </div>
-
                 <!-- tab providers  -->
                 <div role="tabpanel" class="tab-pane" id="providers">
                     <div class="mid_container">
                         <section class="section">
-                            <div class=" " id="feed_table" style="overflow: hidden;">
-                                <div class="pull-left search mb-2">
-                                    <input class="form-control" type="text" placeholder="Buscar">
-                                </div>
-                                <div class="pull-right search mb-2">
+                                <div class="btn-tools-bar">
                                     <button type="button" id="addProvider" class="btn btn-info ml-5" data-toggle="modal"
                                         data-target="#addProviderModal"><i class="fa fa-plus" aria-hidden="true"></i>
                                         Añadir Proveedor</button>
                                 </div>
-                                <table class="table table-striped table-hover bg-white text-center" id="provider_table"
-                                    style="width:99.9%;">
+                                <table class="bg-white" id="provider_table"
+                                    data-toggle="table"
+                                    data-classes="table table-striped table-hover table-borderless"
+                                    data-pagination="true"
+                                    data-search="true">
                                     <thead class="thead-primary">
                                     
                                         <tr>
@@ -145,79 +209,64 @@
                                         @endforeach
                                     </tbody>
                                 </table>
-
-                            </div>
                         </section>
                     </div>
                     <!-- Modals Providers -->
                     @include('spark::modals.resource.providerModals')
 
                 </div>
-                <!-- labs -->
+                <!-- laboratories -->
                 <div role="tabpanel" class="tab-pane" id="labs">
                     <div class="mid_container">
                         <section class="section">
-                            <div class=" " id="feed_table" style="overflow: hidden;">
-                                <div class="pull-left search mb-2">
-                                    <input class="form-control" type="text" placeholder="Buscar">
-                                </div>
-                                <div class="pull-right search mb-2">
+                                <div class="btn-tools-bar">
                                     <button type="button" id="addLab" class="btn btn-info ml-5" data-toggle="modal"
                                         data-target="#addLabModal"><i class="fa fa-plus" aria-hidden="true"></i>
                                         Añadir Laboratorio</button>
                                 </div>
-                                <table class="table table-striped table-hover bg-white text-center" id="Labs-table"
-                                    style="width:99.9%;">
+                                <table class="bg-white" id="Labs-table"
+                                    data-toggle="table"
+                                    data-classes="table table-striped table-hover table-borderless"
+                                    data-pagination="true"
+                                    data-search="true">
                                     <thead class="thead-primary">
                                         <tr>
-                                            <th class="" scope="col">
-                                                <div class="tablesorter-header-inner">Nombre del Laboratorio</div>
-                                            </th>
-                                            <th class="">
-                                                <div class="tablesorter-header-inner">Teléfono</div>
-                                            </th>
-                                            <th class="">
-                                                <div class="tablesorter-header-inner">Email</div>
-                                            </th>
-                                            <th class="">
-                                                <div class="tablesorter-header-inner">Dirección</div>
-                                            </th>
-                                            <th class="">
-                                                <div class="tablesorter-header-inner">Acciones</div>
-                                            </th>
+                                            <th>Nombre del Laboratorio</th>
+                                            <th>Teléfono</th>
+                                            <th>Email</th>
+                                            <th>Dirección</th>
+                                            <th>Acciones</th>
                                         </tr>
                                     </thead>
                                     <tbody class="results" aria-live="polite" aria-relevant="all">
+                                    @foreach($laboratories as $laboratory)
                                         <tr class="scrl_tr" role="row">
-                                            <td class="filter_row">
-                                                <span class="tank_filter">Laboratorio 1</span>
-
-                                            </td>
-                                            <td class="filter_row">
-                                                <span class="tank_filter">5434323343</span>
-
-                                            </td>
-                                            <td class="filter_row">
-                                                <span class="tank_filter">laboratorio1@gmail.com</span>
-                                            </td>
-                                            <td class="filter_row">
-                                                <span class="tank_filter">La dirección</span>
-                                            </td>
+                                            <td>{{$laboratory->name}}</td>
+                                            <td>{{$laboratory->phone}}</td>
+                                            <td>{{$laboratory->email}}</td>
+                                            <td>{{$laboratory->address}}</td>
                                             <td class="text-center">
                                                 <div class="actions btn btn-group-sm">
                                                     <button id="#editLab" data-toggle="modal"
-                                                        data-target="#editLabModal" class="btn btn-success btn-xs mr-4">
+                                                        data-target="#editLabModal"
+                                                        data-id="{{$laboratory->id}}"
+                                                        data-name="{{$laboratory->name}}"
+                                                        data-phone="{{$laboratory->phone}}"
+                                                        data-email="{{$laboratory->email}}"
+                                                        data-address="{{$laboratory->address}}"
+                                                        class="btn btn-success btn-xs mr-4">
                                                         <i class="fa fa-edit"></i></button>
-                                                    <button data-toggle="modal" data-target="#deleteLabModal"
+                                                    <button data-toggle="modal"
+                                                    data-target="#deleteLabModal"
+                                                    data-id="{{$laboratory->id}}"
                                                         class="btn btn-xs btn-danger">
                                                         <i class="fa fa-trash-o"></i></button>
                                                 </div>
                                             </td>
                                         </tr>
+                                        @endforeach
                                     </tbody>
                                 </table>
-
-                            </div>
                         </section>
                     </div>
                     <!-- Modals Labs -->
--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,22 @@ Route::resource('/cultivation', 'CultivationController');
 Route::get('presentation/{resource_id}','CultivationController@getPresentationResource');
 Route::resource('/sowing', 'SowingController');
 Route::resource('/resource', 'ResourceController');
+
 Route::post('/resource/providers', 'ResourceController@storeProvider')->name('storeProvider');
-Route::get('/resource/providers', 'ResourceController@showProvider')->name('showProvider');;
-Route::patch('/resource/providers/{provider_id}', 'ResourceController@updateProvider')->name('updateProvider');;
-Route::delete('/resource/providers/{provider_id}', 'ResourceController@destroyProvider')->name('destroyProvider');;
+Route::get('/resource/providers', 'ResourceController@showProvider')->name('showProvider');
+Route::patch('/resource/providers/{provider_id}', 'ResourceController@updateProvider')->name('updateProvider');
+Route::delete('/resource/providers/{provider_id}', 'ResourceController@destroyProvider')->name('destroyProvider');
+
+Route::post('/resource/presentations', 'ResourceController@storePresentation')->name('storePresentation');
+Route::get('/resource/presentations', 'ResourceController@showPresentation')->name('showPresentation');
+Route::patch('/resource/presentations/{presentation_id}', 'ResourceController@updatePresentation')->name('updatePresentation');
+Route::delete('/resource/presentations/{presentation_id}', 'ResourceController@destroyPresentation')->name('destroyPresentation');
+
+Route::post('/resource/laboratories', 'ResourceController@storeLaboratory')->name('storeLaboratory');
+Route::get('/resource/laboratories', 'ResourceController@showLaboratory')->name('showLaboratory');
+Route::patch('/resource/laboratories/{lab_id}', 'ResourceController@updateLaboratory')->name('updateLaboratory');
+Route::delete('/resource/laboratories/{lab_id}', 'ResourceController@destroyLaboratory')->name('destroyLaboratory');
+
 
 
 
